6ddf2570a21f8243db1a8dc2efad69d0bb7ae635
Rafael Justino <rafael01justino@gmail.com>
2021-07-10 16:54:51 -0300

    Removed unused variables

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_datawedge/flutter_datawedge.dart';
