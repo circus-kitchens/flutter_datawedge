5377d118dca17f5fc6074a8fd775ed4c2242119f
Yannick Stolle <yannick.stolle@merckgroup.com>
2023-08-18 12:50:55 +0200

    chore: cleanup, docs

--- lib/src/consts/method_channel_methods.dart
+++
@@ -1,9 +0,0 @@
-enum MethodChannelMethods {
-  createDataWedgeProfile("createDataWedgeProfile"),
-  listenScannerStatus("listenScannerStatus"),
-  sendDataWedgeCommandStringParameter("sendDataWedgeCommandStringParameter");
-
-  final String value;
-
-  const MethodChannelMethods(this.value);
-}
