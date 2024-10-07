fe22716659609ea81dc8ea9619f9d99b6e23a71d
Tobias Ottenweller <tobias@circuskitchens.com>
2024-10-07 16:51:12 +0200

    Update example project

--- example/android/app/src/main/kotlin/com/justino/flutter_datawedge_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.circuskitchens.flutter_datawedge_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ example/test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_datawedge_example/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+  });
+}
